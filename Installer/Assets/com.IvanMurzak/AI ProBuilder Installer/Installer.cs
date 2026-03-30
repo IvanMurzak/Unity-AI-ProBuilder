ffd75e796fcd4d02ababd05dd0aa92c86c716cd3
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-03-29 17:40:09 -0700

    AI Game Developer update

--- Installer/Assets/com.IvanMurzak/AI ProBuilder Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/AI ProBuilder Installer/Installer.cs
@@ -19,7 +19,7 @@ namespace com.IvanMurzak.Unity.MCP.ProBuilder.Installer
     public static partial class Installer
     {
         public const string PackageId = "com.ivanmurzak.unity.mcp.probuilder";
-        public const string Version = "1.0.58";
+        public const string Version = "1.0.59";
 
         static Installer()
         {
